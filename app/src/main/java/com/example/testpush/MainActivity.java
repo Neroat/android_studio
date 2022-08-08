82cc534303ccc51c30bc8e49ba42962bc61dd180
MyCom <kikahi@naver.com>
2022-08-08 21:19:09 +0900

    test

--- app/src/main/java/com/example/testpush/MainActivity.java
+++ app/src/main/java/com/example/testpush/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.testpush;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv = findViewById(R.id.tv);
+
+                Random rd = new Random();
+                String rdText = "가나다라마바사아자차카타파하";
+                tv.setText("");
+                String show = "";
+
+                for(int i = 0; i<5; i++)
+                {
+                    char rdC = rdText.charAt(rd.nextInt(rdText.length()));
+                    show = show+rdC;
+                }
+                tv.setText(show);
     }
 }
